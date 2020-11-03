d04cc75a10a7b5e42905ba09913dfdc6e8842cd2
zhugeyicixin <tanjin_he@berkeley.edu>
2020-11-02 16:58:03 -0800

    code cleaning

--- MatEntityRecognition/materials_entity_recognition/__init__.py
+++ MatEntityRecognition/materials_entity_recognition/__init__.py
@@ -1,6 +1,9 @@
 from .scripts import MatIdentification
 from .scripts import MatRecognition
 from .scripts import MatTPIdentification
+from .scripts import MatIdentificationBagging
+from .scripts import MatRecognitionBagging
+from .scripts import MatTPIdentificationBagging
 
 __author__ = 'Tanjin He'
 __maintainer__ = 'Tanjin He'
